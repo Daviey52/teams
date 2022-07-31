554e78c6911dd54b1351e1e998ca1f780cbd7483
Daviey52 <davidkariska@gmail.com>
2022-07-31 13:30:38 -0700

    functional list and detail page

--- teams/models.py
+++ teams/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.urls import reverse
 
 class Teams(models.Model):
   name = models.CharField(max_length=64)
@@ -8,3 +9,6 @@ class Teams(models.Model):
 
   def __str__(self):
     return self.name
+
+  def get_absolute_url(self):
+    return reverse ('teams_detail', arg=[str(self.id)])
